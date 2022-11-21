bf410d005154d48e5bb580d752254f3efc46ca54
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-11-21 18:54:10 +0300

    add react toolkit slice

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React, { FC } from "react";
+import { userSlice } from "./store/reducers/UserSlice";
+import { useAppDispatch, useAppSelector } from "./hooks/redux";
 
 const App: FC = () => {
-  return <div>It works!</div>;
+  const {count} = useAppSelector(state => state.userReducer)
+  const {increment} = userSlice.actions
+  const dispatch = useAppDispatch()
+  
+  return (
+  <div>
+    <h1>{count}</h1>
+    <button onClick={() => dispatch(increment(1))}>INCREMENT</button>
+  </div>
+  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { setupStore } from "./store/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={setupStore()}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
