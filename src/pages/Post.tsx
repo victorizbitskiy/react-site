f7f413417500772e873ffc03092689109e885657
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-12-22 22:56:28 +0300

    add Rate to Post.tsx

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,20 +1,36 @@
-import { Breadcrumb, Layout, Spin } from 'antd';
-import {FC, useEffect} from 'react';
+import { Breadcrumb, Layout, Spin, Rate} from 'antd';
+import React, {FC, useEffect, useState} from 'react';
+import { FrownOutlined, MehOutlined, SmileOutlined } from '@ant-design/icons';
 import { useParams, Link } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../hooks/redux';
 import { fetchPostById } from '../store/reducers/ActionCreators';
 import Title from 'antd/es/typography/Title';
 import '../pages/pages.css'
 
+const customIcons: Record<number, React.ReactNode> = {
+  1: <FrownOutlined />,
+  2: <FrownOutlined />,
+  3: <MehOutlined />,
+  4: <SmileOutlined />,
+  5: <SmileOutlined />,
+};
+
 const Post: FC = ()  => {
   const {id} =  useParams<{id: string}>()
   const dispatch = useAppDispatch()
+  const [rateValue, setRateValue] = useState(null)
 
   useEffect(() =>{
     dispatch(fetchPostById(Number(id)))
+    setRateValue(+localStorage.getItem(id))
   }, [])
 
   const {post, isLoading, error} = useAppSelector(state => state.postReducer)
+ 
+  const setRateValueToLocalStorage = ( value: number ) => {
+    setRateValue(value)
+    localStorage.setItem(post.id.toString(), value.toString())
+  }
 
   return (
     <Layout className='Pages-layout'>
@@ -33,11 +49,16 @@ const Post: FC = ()  => {
             :
             <div>
               <Title level={2}>{post.id}. {post.title}</Title>
-
               <div>
                 {post.body}
               </div>
             </div>}
+
+          <Rate 
+          character={({ index }: { index: number }) => customIcons[index + 1]}
+          onChange={setRateValueToLocalStorage} 
+          value={rateValue}
+           />
         </Layout.Content>
       </Layout>
   );
